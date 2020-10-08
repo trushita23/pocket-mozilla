4c869f74b8b1f99fff10f08843dd82ea69e7e13d
trushita23 <trushita@gmail.com>
2020-10-09 00:24:08 +0530

    add logo to center

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import pocket from './Pocket_Logo_Small.png';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+  root: {
+    display:'block',
+    marginLeft: 'auto',
+    marginRight:'auto'
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <img src={pocket} className="App-logo" alt="pocket_logo" />
+        <Box m={2}>
+          <Button variant="contained" color="secondary" className={classes.root}>Trushita Maurya</Button>
+        </Box>
+        <div>
+        </div>
       </header>
     </div>
   );
